f77d398bc83cdeb4723e1b7f9c229a8e61c2a2c0
Kazutoshi Umemoto <umemoto@tkl.iis.u-tokyo.ac.jp>
2021-07-27 05:35:44 +0900

    feat: Use official Homebrew installation script to benefit from binary packages

--- dot/zsh/inits/50-brew.sh
+++ dot/zsh/inits/50-brew.sh
@@ -1,5 +1,11 @@
-HOMEBREW_PREFIX="${HOME}/.linuxbrew"
+if [ -d "${HOME}/.linuxbrew" ]; then
+    HOMEBREW_PREFIX="${HOME}/.linuxbrew"
+elif [ -d "/home/linuxbrew/.linuxbrew" ]; then
+    HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
+else
+    HOMEBREW_PREFIX=""
+fi
 
-if [ -x "${HOMEBREW_PREFIX}/bin/brew" ]; then
+if [ -n "${HOMEBREW_PREFIX}" ] && [ -x "${HOMEBREW_PREFIX}/bin/brew" ]; then
     eval $("${HOMEBREW_PREFIX}/bin/brew" shellenv)
 fi
--- etc/50-brew.sh
+++ etc/50-brew.sh
@@ -12,12 +12,17 @@ if type brew >/dev/null 2>&1; then
     echo_warning "Skip brew installation as it already exists."
 else
     echo_info "Installing brew itself..."
-    run_verbosely git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
-    run_verbosely mkdir ~/.linuxbrew/bin
-    run_verbosely ln -s ~/.linuxbrew/Homebrew/bin/brew ~/.linuxbrew/bin
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
     echo_info "Export brew-specific variables"
-    eval $(~/.linuxbrew/bin/brew shellenv)
+    if [ -d ~/.linuxbrew ]; then
+        eval $(~/.linuxbrew/bin/brew shellenv)
+    elif [ -d /home/linuxbrew/.linuxbrew ]; then
+        eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+    else
+        echo_error "Failed to locate brew prefix path."
+        exit 1
+    fi
 
     echo_info "Adding brew repositories..."
     run_verbosely brew tap "homebrew/core"
